d968f4bb14949c5642f9812a660c2dc990b258a2
nwlilley <nathaniel.lilley@gmail.com>
2020-06-17 16:45:59 -0400

    pass with errors

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,111 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import store from "@/store";
+Vue.use(Router);
 
-Vue.use(VueRouter);
+const router = new Router({
+  mode: "history",
+  linkExactActiveClass: "vue-school-active-class",
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      const position = {};
+      if (to.hash) {
+        position.selector = to.hash;
+        if (to.hash === "#experience") {
+          position.offset = { y: 140 };
+        }
+        if (document.querySelector(to.hash)) {
+          return position;
+        }
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
+        return false;
+      }
+    }
   },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+      props: true
+    },
 
-const router = new VueRouter({
-  routes
+    {
+      path: "/destination/:slug",
+      name: "DestinationDetails",
+      props: true,
+      component: () =>
+        import(/* webpackChunkName: "DestinationDetails"*/ "../views/DestinationDetails"),
+      children: [
+        {
+          path: ":experienceSlug",
+          name: "experienceDetails",
+          props: true,
+          component: () =>
+            import(/*webpackChunkName: "ExperienceDetails"*/ "./views/ExperienceDetails")
+        }
+      ],
+      beforeEnter: (to, from, next) => {
+        const exists = store.destinations.find(
+          destination => destination.slug === to.params.slug
+        );
+        if (exists) {
+          next();
+        } else {
+          next({ name: "notFound" });
+        }
+      }
+    },
+    {
+      path: "/user",
+      name: "user",
+      component: () =>
+        import(/* webpackChunkName: "User" */ "./views/User.vue"),
+      meta: { requiresAuth: true }
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: () =>
+        import(/* webpackChunkName: "Login" */
+        "./views/Login.vue")
+    },
+    {
+      path: "/invoices",
+      name: "invoices",
+      component: () =>
+        import(/* webpackChunkName: "Invoices" */ "./views/Invoices"),
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/404",
+      alias: "*",
+      name: "notFound",
+      component: () =>
+        import(/* webpackChunkName: "NotFound" */
+        "./views/NotFound")
+    }
+  ]
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.user) {
+      next({
+        name: "login",
+        query: { redirect: to.fullPath }
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
